920fa4130c1774254eb733795353316f22056ec0
Geeva veeri <cbr_me@yahoo.com>
2021-03-19 19:05:00 -0600

    added playerName var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! Javascript is running!");
+//This creates the function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+
+fight();
